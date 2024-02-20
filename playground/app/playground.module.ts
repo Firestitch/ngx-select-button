81c0bc40daed9f59a5b9424a37aa0130aa1250c9
Raymond Gigliotti <ray@firestitch.com>
2024-02-20 15:30:43 -0500

    A13

--- playground/app/playground.module.ts
+++ playground/app/playground.module.ts
@@ -1,22 +1,21 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule, Routes } from '@angular/router';
 
 import { MatButtonModule } from '@angular/material/button';
 
-import { FsSelectButtonModule } from '@firestitch/select-button';
-import { FsExampleModule } from '@firestitch/example';
-import { FsMessageModule } from '@firestitch/message';
 import { FsDrawerModule } from '@firestitch/drawer';
+import { FsExampleModule } from '@firestitch/example';
 import { FsLabelModule } from '@firestitch/label';
+import { FsMessageModule } from '@firestitch/message';
+import { FsSelectButtonModule } from '@firestitch/select-button';
 
 import { AppComponent } from './app.component';
-import { AppMaterialModule } from './material.module';
 import { ExampleComponent, ExamplesComponent } from './components';
 import { ConfigureComponent } from './components/configure';
+import { AppMaterialModule } from './material.module';
 
 
 const routes: Routes = [
@@ -24,29 +23,27 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    bootstrap: [AppComponent],
-    imports: [
-        BrowserModule,
-        FsSelectButtonModule,
-        BrowserAnimationsModule,
-        AppMaterialModule,
-        FormsModule,
-        FlexLayoutModule,
-        MatButtonModule,
-        MatButtonModule,
-        FsDrawerModule,
-        FsLabelModule,
-        RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
-        FsMessageModule.forRoot(),
-        FsExampleModule.forRoot(),
-    ],
-    declarations: [
-        AppComponent,
-        ExamplesComponent,
-        ExampleComponent,
-        ConfigureComponent
-    ],
-    providers: []
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    FsSelectButtonModule,
+    BrowserAnimationsModule,
+    AppMaterialModule,
+    FormsModule,
+    MatButtonModule,
+    MatButtonModule,
+    FsDrawerModule,
+    FsLabelModule,
+    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
+    FsMessageModule.forRoot(),
+    FsExampleModule.forRoot({ iframeObserveBody: true }),
+  ],
+  declarations: [
+    AppComponent,
+    ExamplesComponent,
+    ExampleComponent,
+    ConfigureComponent
+  ],
 })
 export class PlaygroundModule {
 }
